1a27a56d90a2cbfe7269a2ad079b4f816eaf43a5
EJ Brown <ejbrown3c@gmail.com>
2021-10-14 17:11:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+const activity = "bathe my dog"
+
+
+function mondayWork ( activity1 = "go to the office") {
+  return (`This Monday, I will ${activity1}.`)
+}
+
+const activity1 = "work from home"
+
+
+
+function wrapAdjective(typeEncourage = "*") {
+    return function inner (quote = "special") {
+        return (`You are ${typeEncourage}${quote}${typeEncourage}!`)
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
